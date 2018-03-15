f48acb8438f070c93e906556d6b2a870d5573728
matthewcoledesign@gmail.com <matthewcoledesign@gmail.com>
2018-03-14 20:08:28 -0600

    Add GET /todos:/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -41,6 +42,26 @@ app.get('/todos', (req, res) => {
 // });
 
 // GET /todos/123a;skldjf
+
+
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+    return;
+  }
+    Todo.findById(id).then((todo) =>{
+      if(!todo){
+        res.status(404).send();
+        return;
+      }
+      res.send({todo});
+    }, (e) =>{
+      res.status(400).send();
+    });
+});
+
 
 app.listen(3000, () => {
   console.log(`Started on port 3000`);
