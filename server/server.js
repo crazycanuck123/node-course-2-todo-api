ccc5db7c53638378f2dd2f661d5bcd2c652a968a
matthewcoledesign@gmail.com <matthewcoledesign@gmail.com>
2018-03-24 22:27:17 -0600

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var {authenticate} = require ('./middleware/authenticate');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -109,11 +110,16 @@ app.post('/users', (req, res) =>{
 
   user.save().then(() =>{
     return user.generateAuthToken();
-  }, (e) =>{
-    res.status(400).send(e);
   }).then((token) =>{
     res.header('x-auth', token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
   });
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
