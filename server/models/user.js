685650fb7e8db32468cd58f3df6e88593add4963
matthewcoledesign@gmail.com <matthewcoledesign@gmail.com>
2018-03-27 21:16:50 -0600

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -66,6 +67,26 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function (next){
+  var user = this;
+  // bcrypt.genSalt(10, (err, salt) => {
+  //   bcrypt.hash(password, salt, (err, hash) => {
+  //     console.log(hash);
+  //   });
+  // });
+if(user.isModified('password')) {
+  //user.password
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      user.password = hash;
+      next();
+    });
+  });
+}else{
+  next();
+}
+});
 
 var User = mongoose.model('User', UserSchema);
 
